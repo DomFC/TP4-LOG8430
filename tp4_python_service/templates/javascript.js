3b2f12620c0a32248697bf3488e28fe0b3a13d4a
Kevin Pantelakis <kev.pantelakis@gmail.com>
2018-11-19 18:25:01 -0500

    voila

--- tp4_python_service/templates/javascript.js
+++ tp4_python_service/templates/javascript.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $("#submitPostButton").click(function(){
-        alert( $("#invoice").val());
         $.post("http://127.0.0.1:5000/invoice",
         $("#invoice").val(),
         function(data, status){
